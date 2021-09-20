4215b9a946eda5f52231a864cd62ca9400e38a69
Rey <3065588496@qq.com>
2021-09-20 12:16:01 +0800

    Added systemtray

--- config.h
+++ config.h
@@ -17,6 +17,15 @@ static int swallowfloating =
     0; /* 1 means swallow floating windows by default */
 static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning =
+    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+          X */
+static const unsigned int systrayonleft =
+    0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst =
+    1; /* 1: if pinning fails, display systray on the first monitor, False:
+          display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
 static int showbar = 1;           /* 0 means no bar */
 static int topbar = 1;            /* 0 means bottom bar */
